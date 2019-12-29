5b7320763b9e03963d04f4f30d4caba8038af540
Signatuz <brunoxz.10x@hotmail.com>
2019-12-29 14:24:09 -0300

    Segundo projeto

---
+++ Python/biblioteca.py
@@ -0,0 +1,9 @@
+# -*- coding:  UTF-8 -*-
+
+class Perfil():
+
+    def __init__(self, nome, telefone, empresa):
+        self.nome = nome
+        self.telefone = telefone
+        self.empresa = empresa
+    
